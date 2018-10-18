8fe769204ef1aae7cddd740f2455af192d36ac1c
PhyzXeno <630172407@qq.com>
2018-10-18 13:45:57 +0800

    modify my_ip.py

--- my_ip.py
+++ my_ip.py
@@ -4,5 +4,5 @@ import re
 url = "http://ip4.me"
 r = requests.get(url)
 result = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b",str(r.content))
-print("Your IP address is:",result[0])
+print("Current IP address is:",result[0])
 
